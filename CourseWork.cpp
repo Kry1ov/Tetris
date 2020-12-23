ea68a76d070d54942e899f66f1e7743bd1211743
Kry1ov <71841167+Kry1ov@users.noreply.github.com>
2020-12-23 21:34:41 +0300

    Add files via upload

---
+++ CourseWork.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <exception>
+
+#include "game.h"
+using namespace GameSpace;
+
+int main()
+{
+	try
+	{
+		Game tetris(640, 480, false);
+		tetris.doWork();
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << std::endl;
+	}
+	return 0;
+}
